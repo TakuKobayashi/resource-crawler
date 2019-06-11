00444ac9672fe0d74f5eebf331e7f733c93a0948
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-06-12 01:36:57 +0900

    flickerから情報を取得できるようにした

--- lambda/flickr/search/reaources/index.js
+++ lambda/flickr/search/reaources/index.js
@@ -1,9 +1,18 @@
+const requireRoot = require('app-root-path').require;
+const frickrSearch = requireRoot("/libs/frickrSearch");
+const apiRenderTemplate = requireRoot("/libs/apiRenderTemplate");
+
 exports.handler = async (event, context) => {
-    console.log(event);
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return event;
+  const startTime = new Date();
+  console.log(event);
+  const serachParams = {}
+  if(event.q){
+    serachParams.text = event.q;
+    delete event.q;
+  }else if(!event.user_id && !event.tags){
+    return apiRenderTemplate("failed", startTime, {images: []});
+  }
+  const allSearchResults = await frickrSearch.searchAllFlickrPhotos(Object.assign(serachParams, event));
+
+  return apiRenderTemplate("success", startTime, {images: allSearchResults});
 };
