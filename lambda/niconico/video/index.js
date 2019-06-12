5e0dafe208ab282ad558b54ead9056b5901d1594
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-06-12 19:16:08 +0900

    niconico動画から動画を取得できる情報を含んだAPIの作成

--- lambda/niconico/video/index.js
+++ lambda/niconico/video/index.js
@@ -1,9 +1,23 @@
+const requireRoot = require('app-root-path').require;
+const niconicoResource = requireRoot("/libs/niconicoResource");
+const apiRenderTemplate = requireRoot("/libs/apiRenderTemplate");
+
 exports.handler = async (event, context) => {
-    console.log(event);
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return event;
+  const startTime = new Date();
+  console.log(event);
+  if (!event.urls) {
+    return apiRenderTemplate("failed", startTime, {videos: []});
+  }
+  const requestUrls = event.urls.split(",");
+  const allScrapeResults = [];
+  for (const requestUrl of requestUrls){
+    const scrapeResults = await niconicoResource.scrapeResourceFromUrl(requestUrl);
+    for (const scrapeResult of scrapeResults){
+      allScrapeResults.push(scrapeResult);
+    }
+  }
+
+  return apiRenderTemplate("success", startTime, {
+      videos: allScrapeResults
+  });
 };
---
+++ libs/niconicoResource.js
@@ -0,0 +1,32 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+exports.scrapeResourceFromUrl = async function scrapeResourceFromUrl(url) {
+  const niconicoPageResponse = await axios.get(url);
+  const niconicoPageHeaders = niconicoPageResponse.headers['set-cookie'] || [];
+  const shouldAttachCookieHeaderValue = niconicoPageHeaders.find(cookie => cookie.includes("nicohistory="))
+  const $ = cheerio.load(niconicoPageResponse.data);
+  const results = [];
+
+  for (const element of Object.values($("#js-initial-watch-data"))) {
+    const apiData = $(element).data("api-data");
+    if(!apiData) continue;
+    const videoMeta = apiData.video
+
+    results.push({
+      id: videoMeta.id.toString(),
+      title: videoMeta.title,
+      description: videoMeta.description,
+      duration_millis: videoMeta.duration,
+      thumbnail_image_url: videoMeta.largeThumbnailURL || videoMeta.thumbnailURL,
+      posted_date_time: videoMeta.postedDateTime,
+      should_attach_cookie_value: shouldAttachCookieHeaderValue,
+      videos: [
+        {
+          url: videoMeta.smileInfo.url
+        }
+      ],
+    });
+  }
+  return results;
+}
