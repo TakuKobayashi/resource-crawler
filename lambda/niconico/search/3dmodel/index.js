fbbf229f7d039eed265f0c219b79f1408b3e028b
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2019-06-16 02:09:50 +0900

    ニコニ立体は別物として一旦考える

--- lambda/niconico/search/3dmodel/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  console.log(event);
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return event;
-};
