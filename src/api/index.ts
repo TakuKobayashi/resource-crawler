9da997853433c3e02a7fbbbafb2712e830f63b2a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-01 02:34:04 +0900

    hash値の計算を試してみた

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import serverlessExpress from '@vendia/serverless-express';
 import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import { getHashes , createHash } from 'crypto';
 
 import { flickrSearchRouter } from './routes/flickr/search';
 import { googleSearchRouter } from './routes/google/search';
@@ -36,6 +37,22 @@ app.use('/youtube/video', youtubeVideoRouter);
 app.get('/test', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     message: 'Hello from root!',
+  });
+});
+
+app.get('/crypttest', (req: Request, res: Response, next: NextFunction) => {
+  const queriesJson = JSON.stringify(req.query);
+  // 以降は https://qiita.com/koki_develop/items/174aefd8f894fea4d11a を参照
+  // `crypto.createHash('アルゴリズム名').update('文字列').digest('hex')`
+  res.json({
+    // サポートされているハッシュアルゴリズムを確認
+    // getHashes: getHashes(),
+    md5: createHash('md5').update(queriesJson).digest('hex'),
+    rmd160: createHash('rmd160').update(queriesJson).digest('hex'),
+    sha1: createHash('sha1').update(queriesJson).digest('hex'),
+    sha256: createHash('sha256').update(queriesJson).digest('hex'),
+    sha384: createHash('sha384').update(queriesJson).digest('hex'),
+    sha512: createHash('sha512').update(queriesJson).digest('hex'),
   });
 });
 
