69d59668a1598c4d10743443079563d5efd4f8da
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-02 10:37:05 +0900

    jimpを用いて画像のハッシュ値を抽出する試み

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import serverlessExpress from '@vendia/serverless-express';
 import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
-import { getHashes, createHash } from 'crypto';
+import { createHash } from 'crypto';
+import path from 'path';
+import jimp from 'jimp';
 
 import { flickrSearchRouter } from './routes/flickr/search';
 import { googleSearchRouter } from './routes/google/search';
@@ -58,6 +60,7 @@ app.get('/crypttest', (req: Request, res: Response, next: NextFunction) => {
 
 // 画像のハッシュpHashの各種ライブラリを試してみる
 app.get('/checkimage_hashes', async (req: Request, res: Response, next: NextFunction) => {
+  const srcDir = path.basename(path.dirname(__dirname));
   // 検証用で用意した画像たち
   // https://taptappun.s3.ap-northeast-1.amazonaws.com/sample_auto_filter.jpg
   // https://taptappun.s3.ap-northeast-1.amazonaws.com/sample_crop.jpg
@@ -65,7 +68,20 @@ app.get('/checkimage_hashes', async (req: Request, res: Response, next: NextFunc
   // https://taptappun.s3.ap-northeast-1.amazonaws.com/sample_origin_small_size.jpg
   // https://taptappun.s3.ap-northeast-1.amazonaws.com/sample_sometime_filter.jpg
   // https://taptappun.s3.ap-northeast-1.amazonaws.com/sample_vertical.jpg
+  const sample_origin_jpg = await jimp.read(path.join(srcDir, 'sample_images', 'sample_origin.jpg'));
+  const sample_origin_small_size_jpg = await jimp.read(path.join(srcDir, 'sample_images', 'sample_origin_small_size.jpg'));
+  const sample_sometime_filter_jpg = await jimp.read(path.join(srcDir, 'sample_images', 'sample_sometime_filter.jpg'));
+  const sample_vertical_jpg = await jimp.read(path.join(srcDir, 'sample_images', 'sample_vertical.jpg'));
+  const sample_crop_jpg = await jimp.read(path.join(srcDir, 'sample_images', 'sample_crop.jpg'));
+  const sample_auto_filter_jpg = await jimp.read(path.join(srcDir, 'sample_images', 'sample_auto_filter.jpg'));
   res.json({
+    // 16進数
+    sample_origin_jpg: parseInt(sample_origin_jpg.pHash(), 2).toString(16),
+    sample_origin_small_size_jpg: parseInt(sample_origin_small_size_jpg.pHash(), 2).toString(16),
+    sample_sometime_filter_jpg: parseInt(sample_sometime_filter_jpg.pHash(), 2).toString(16),
+    sample_vertical_jpg: parseInt(sample_vertical_jpg.pHash(), 2).toString(16),
+    sample_crop_jpg: parseInt(sample_crop_jpg.pHash(), 2).toString(16),
+    sample_auto_filter_jpg: parseInt(sample_auto_filter_jpg.pHash(), 2).toString(16),
   });
 });
 
