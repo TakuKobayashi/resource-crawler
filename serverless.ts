91550eaf5bca8f604dc8e6ab9b801fcee503a50a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-07-18 12:08:13 +0900

    region を指定しない

--- serverless.ts
+++ serverless.ts
@@ -58,7 +58,7 @@ const serverlessConfiguration: AWS = {
           ],
           Resource: [
             // 'arn:aws:dynamodb:[region名]:[accountid]:table/[テーブル名]'
-            'arn:aws:dynamodb:ap-northeast-1:*:table/*'
+            'arn:aws:dynamodb:*:*:table/*'
           ],
         }],
       },
