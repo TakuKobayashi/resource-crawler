8de8c74d84593bc6ffd1111105cab8563d773b6c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-04-24 05:11:52 +0900

    データベースの文字コードを設定した

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -7,6 +7,11 @@ const config = {
   port: process.env.MYSQL_PORT,
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_ROOT_PASSWORD,
+  define: {
+    charset: process.env.MYSQL_CHARSET,
+    collate: process.env.MYSQL_COLLATE,
+    encoding: process.env.MYSQL_ENCODING,
+  },
 };
 
 module.exports = config;
