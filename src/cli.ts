dab5ca534c89fa916187c554753a9303be88b804
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-04-25 17:35:05 +0900

    新しいデータ構造のデータを一括で登録するようにした

--- src/cli.ts
+++ src/cli.ts
@@ -7,6 +7,7 @@ import { searchInstagramImagesFromUserName } from './libs/services/instagram/htm
 import models from './sequelize/models';
 import { config } from 'dotenv';
 config();
+import { v4 as uuidv4 } from 'uuid';
 import { ServiceTypes } from './sequelize/enums/service-types';
 import { WordTypes } from './sequelize/enums/word-types';
 import { ResourceTypes } from './sequelize/enums/resource-types';
@@ -22,6 +23,12 @@ program.version(packageJson.version, '-v, --version');
 const crawlCommand = new Command('crawl');
 crawlCommand.description('crawl');
 
+interface ExecuteResultModels {
+  resources: any[];
+  contents: any[];
+  resourceContents: any[];
+}
+
 const searchKeywordRoutine = async ({
   serviceType,
   wordType,
@@ -31,7 +38,7 @@ const searchKeywordRoutine = async ({
   serviceType: ServiceTypeNames;
   wordType: WordTypeNames;
   word: string;
-  execution: (keyword) => {};
+  execution: (keyword) => Promise<ExecuteResultModels>;
 }) => {
   const [keyword, isCreated] = await models.Keyword.findOrCreate({
     where: {
@@ -40,8 +47,9 @@ const searchKeywordRoutine = async ({
       word: word,
     },
   });
-  const resourceModels = await execution(keyword);
-  await models.Resource.bulkCreate(resourceModels);
+  const contentResources = await execution(keyword);
+  await models.Content.bulkCreate(contentResources.contents, { updateOnDuplicate: ['website_url'] });
+  await models.Resource.bulkCreate(contentResources.resources, { updateOnDuplicate: ['url'] });
 };
 
 crawlCommand
@@ -56,15 +64,35 @@ crawlCommand
       execution: async (keyword) => {
         const flickrPhotos = await searchFlickrPhotos({ text: keyword.word });
         const flickrImageResources = flickrPhotos.photo.map((flickrPhoto) => convertToPhotoToObject(flickrPhoto));
-        return flickrImageResources.map((flickrImageResource) => {
-          return {
+        const results: ExecuteResultModels = {
+          contents: [],
+          resources: [],
+          resourceContents: [],
+        };
+        for (const flickrImageResource of flickrImageResources) {
+          const uuid = uuidv4();
+          results.contents.push({
+            uuid: uuid,
+            service_type: keyword.service_type,
             title: flickrImageResource.title,
-            content_id: null,
+            website_url: flickrImageResource.website_url,
+            service_content_id: flickrImageResource.id,
+            service_user_id: flickrImageResource.user_id,
+            service_user_name: flickrImageResource.user_name,
+            latitude: flickrImageResource.latitude,
+            longitude: flickrImageResource.longitude,
+          });
+          results.resources.push({
+            uuid: uuid,
             resource_type: ResourceTypes.image,
             url: flickrImageResource.image_url,
-            flickrImageResources,
-          };
-        });
+          });
+          results.resourceContents.push({
+            resource_uuid: uuid,
+            content_uuid: uuid,
+          });
+        }
+        return results;
       },
     });
   });
